a0308fa9eb93b2f7b1202ff7683950683119417f
Yasin Aksu <yasin.aksu@etiya.com>
2021-08-16 11:26:59 +0300

    initial commit

---
+++ backofisbackend/src/main/java/com/omniteam/backofisbackend/BackofisbackendApplication.java
@@ -0,0 +1,13 @@
+package com.omniteam.backofisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofisbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofisbackendApplication.class, args);
+	}
+
+}
---
+++ backofisbackend/src/test/java/com/omniteam/backofisbackend/BackofisbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omniteam.backofisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofisbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
