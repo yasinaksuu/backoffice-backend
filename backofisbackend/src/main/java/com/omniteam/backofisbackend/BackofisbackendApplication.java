1b458568c60770ccebe11fbaffe7e26d50af38eb
Yasin Aksu <yasin.aksu@etiya.com>
2021-08-19 17:10:52 +0300

    enabling cors

--- backofisbackend/src/main/java/com/omniteam/backofisbackend/BackofisbackendApplication.java
+++ backofisbackend/src/main/java/com/omniteam/backofisbackend/BackofisbackendApplication.java
@@ -2,12 +2,29 @@ package com.omniteam.backofisbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackofisbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackofisbackendApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
