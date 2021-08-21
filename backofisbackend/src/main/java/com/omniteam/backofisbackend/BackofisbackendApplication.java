af8b546e21ff2169d87ecc4450234c1c61fad13c
Yasin Aksu <yasin.aksu@etiya.com>
2021-08-21 08:35:28 +0300

    Swagger was enabled and configured for project

--- backofisbackend/src/main/java/com/omniteam/backofisbackend/BackofisbackendApplication.java
+++ backofisbackend/src/main/java/com/omniteam/backofisbackend/BackofisbackendApplication.java
@@ -5,8 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackofisbackendApplication {
 
 	public static void main(String[] args) {
@@ -14,6 +20,7 @@ public class BackofisbackendApplication {
 	}
 
 
+	//Cors
 	@Bean
 	public WebMvcConfigurer corsConfigurer(){
 		return new WebMvcConfigurer() {
@@ -25,6 +32,16 @@ public class BackofisbackendApplication {
 						.allowedOrigins("*");
 			}
 		};
+	}
+
+	//Swagger
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
