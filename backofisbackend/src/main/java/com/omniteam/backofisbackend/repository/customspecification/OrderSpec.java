ff160f74a278ed7058a1ad78b5a48b495443b976
Yasin Aksu <yasin.aksu@etiya.com>
2021-09-13 17:45:05 +0300

    /api/v1/carts/getall endpoint bug fixed

--- backofisbackend/src/main/java/com/omniteam/backofisbackend/repository/customspecification/OrderSpec.java
+++ backofisbackend/src/main/java/com/omniteam/backofisbackend/repository/customspecification/OrderSpec.java
@@ -5,17 +5,46 @@ import com.omniteam.backofisbackend.entity.OrderDetail;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.ListJoin;
+import javax.persistence.criteria.Predicate;
 import java.time.LocalDateTime;
 
 public class OrderSpec {
     public static Specification<Order> getAllByFilter(int customerId, String status, LocalDateTime startDate,LocalDateTime endDate) {
-        return (root, criteriaQuery, criteriaBuilder) -> {
+        return Specification
+                .where(getOrdersByCustomerId(customerId))
+                .or(getOrdersByStatus(status))
+                .or(getOrdersByDateTimeRange(startDate,endDate)
+        );
+    }
+
+    public static Specification<Order> getOrdersByCustomerId(int customerId){
+        return (root, query, criteriaBuilder) -> {
             ListJoin<Order, OrderDetail> orderDetailListJoin = root.joinList("orderDetails");
-          return criteriaBuilder.and(
-                  criteriaBuilder.equal(root.get("customer"),customerId),
-                  criteriaBuilder.equal(root.get("status"),status),
-                  criteriaBuilder.between(root.get("createdDate"),startDate,endDate)
-          );
+            if(customerId == 0){
+                return criteriaBuilder.conjunction();
+            }
+            Predicate equalPredicate = criteriaBuilder.equal(root.get("customer"), customerId);
+            return equalPredicate;
+        };
+    }
+
+    public static Specification<Order> getOrdersByStatus(String status){
+        return (root, query, criteriaBuilder) -> {
+            if(status == null || status.isEmpty()){
+                return criteriaBuilder.conjunction();
+            }
+            Predicate equalPredicate = criteriaBuilder.equal(root.get("status"), status);
+            return equalPredicate;
+        };
+    }
+
+    public static Specification<Order> getOrdersByDateTimeRange(LocalDateTime startDate,LocalDateTime endDate){
+        return (root, query, criteriaBuilder) -> {
+            if(startDate==null && endDate ==null){
+                return criteriaBuilder.conjunction();
+            }
+            Predicate betweenPredicate = criteriaBuilder.between(root.get("createdDate"),startDate,endDate);
+            return betweenPredicate;
         };
     }
 }
