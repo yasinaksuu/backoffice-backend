806852453733ca1afd5b6c811764c48c0a76f68c
Yasin Aksu <yasin.aksu@etiya.com>
2021-09-14 16:52:34 +0300

    /api/v1/attributes/<attributeid>/getattributeterms endpoint finished

--- backofisbackend/src/main/java/com/omniteam/backofisbackend/service/AttributeTermService.java
+++ backofisbackend/src/main/java/com/omniteam/backofisbackend/service/AttributeTermService.java
@@ -1,11 +1,12 @@
 package com.omniteam.backofisbackend.service;
 
 import com.omniteam.backofisbackend.dto.attibute.AttributeTermDTO;
+import com.omniteam.backofisbackend.shared.result.DataResult;
 
 import java.util.List;
 
 public interface AttributeTermService {
 
-    public List<AttributeTermDTO> getByAttributeTermByAttribute(Integer attributeId) ;
+    DataResult<List<AttributeTermDTO>> getByAttributeTermByAttribute(Integer attributeId);
 
-    }
+}
