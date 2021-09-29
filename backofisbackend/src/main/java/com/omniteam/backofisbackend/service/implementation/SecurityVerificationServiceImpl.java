ae12f9778983370cae8de119da41945ced4f01d3
ETIYACORP\seyda.kurt <seyda.kurt@etiya.com>
2021-09-29 17:27:23 +0300

    Security

---
+++ backofisbackend/src/main/java/com/omniteam/backofisbackend/service/implementation/SecurityVerificationServiceImpl.java
@@ -0,0 +1,34 @@
+package com.omniteam.backofisbackend.service.implementation;
+
+
+import com.omniteam.backofisbackend.repository.UserRepository;
+import com.omniteam.backofisbackend.service.SecurityVerificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class SecurityVerificationServiceImpl implements SecurityVerificationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User obtainTokenizedUser(Object principal) {
+        return principal instanceof User ? (User) principal : null;
+    }
+
+
+    @Transactional
+    public com.omniteam.backofisbackend.entity.User inquireLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = authentication != null ? obtainTokenizedUser(authentication.getPrincipal()) : null;
+       com.omniteam.backofisbackend.entity.User user = userRepository.findUserByEmailAndIsActive(userDetails.getUsername(),true);
+        return user;
+    }
+
+}
