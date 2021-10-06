77934f0c830371fbde65e8d2215881c8c4e7bfa1
Yasin Aksu <yasin.aksu@etiya.com>
2021-10-06 15:04:04 +0300

    cart-service removed from repo

--- cart-service/src/main/java/com/backoffice/microservice/cartservice/service/OrderDetailService.java
+++
@@ -1,9 +0,0 @@
-package com.backoffice.microservice.cartservice.service;
-
-import com.backoffice.microservice.cartservice.dto.OrderDetailDto;
-
-import java.util.List;
-
-public interface OrderDetailService {
-    List<OrderDetailDto> getOrderDetailsByOrderId(Integer orderId);
-}
--- cart-service/src/main/java/com/backoffice/microservice/cartservice/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.backoffice.microservice.cartservice.service;
-
-import com.backoffice.microservice.cartservice.dto.OrderDto;
-import com.backoffice.microservice.cartservice.entity.Order;
-import javassist.NotFoundException;
-
-public interface OrderService {
-    OrderDto getById(Integer id);
-}
